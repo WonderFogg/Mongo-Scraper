9e231f4f36c9cf02af890dbd391fc4d68e234ccc
Joe Fogg <GJF610@gmail.com>
2017-12-04 13:03:44 -0500

    updated code and folder routes

--- server.js
+++ server.js
@@ -1,51 +1,74 @@
 // Dependencies
 var express = require("express");
-var mongoose = require("mongoose");
-var expressHandlebars = require("express-handlebars");
 var bodyParser = require("body-parser");
-
+var request = require("request");
+var logger = require("morgan");
+var mongoose = require("mongoose");
+var Note = require("./models/Note.js");
+var Article = require("./models/Article.js");
+var Save = require("./models/Save.js");
+var path = require("path");
+// Instantiate our Express App
+var app = express();
 // set up port to be either the hosts or designated port, or 3000
 var PORT = process.env.PORT || 3000;
 
-// Instantiate our Express App
-var app = express();
 
-// Set up an Express Router
-var router = express.Router();
-
-//  Designate our public folder as a static directory
-app.use(express.static(_dirname + "/public"));
-
-//  Connect Handlebars to our Exopress app
-app.engine("handlebars", expressHandlebars({
-	defaultLayout: "main"
+// Parse application/x-www-form-urlencoded
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
 }));
+app.use(express.static("./public"));
+
+// connect to database
+mongoose.Promise = Promise;
+var dbConnect = process.env.MONGODB_URI || "mongodb://localhost/foxsScrape";
+if(process.env.MONGODB_URI) {
+    mongoose.connect(process.env.MONGODB_URI)
+} else {
+    mongoose.connect(dbConnect);
+}
+// mongodb://foxsScrape:password12@ds119585.mlab.com:19585/heroku_hd8909ql;
+// Connect mongoose to our database
+/* mongoose.connect(dbConnect, function (error) {
+    // Log any errors connecting with mongoose
+    if (error) {
+        console.log(error);
+    }
+    // Or log a success message
+    else {
+        console.log("Mongoose connection is successful");
+    }
+}); */
+var db = mongoose.connection;
+db.on('error',function(err){
+    console.log('Mongoose Error',err);
+});
+db.once('open', function(){
+    console.log("Mongoose connection is successful");
+});
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({
+    defaultLayout: "main"
+}));
+
 app.set("view engine", "handlebars");
 
-//  Use bodyParser in our app
-app.use(bodyParser.urlencoded({
-	extended: false
-}));	
-
-// Have every request go through our router middleware
-app.use(router);
-
-//  If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
-var db = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
-
-//  Connect mongose to out database
-mongoose.connect(db, function(error) {
-	//  Log any errors connecting with mongoose
-	if (error) {
-		console.log(error);
-	}
-	// Or log a success message
-	else {
-		console.log("mongoose connection is successful");
-	}
-});
-// Listen on the port
-app.listen(PORT, function() {
-	console.log("Listening on port:" + PORT);
+app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "views/index.html"));
 });
 
+require("./routes/scrape")(app);
+require("./routes/html.js")(app);
+
+app.get("*", function (req, res) {
+    res.sendFile(path.join(__dirname, "views/index.html"));
+});
+
+
+app.listen(PORT, function () {
+    console.log("App listening on PORT " + PORT);
+});
